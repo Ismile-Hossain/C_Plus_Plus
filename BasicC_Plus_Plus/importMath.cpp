3504b11e45410796cc76d813da3b7e3252aa88d9
Md.Ismile Hossain <40804562+Ismile-Hossain@users.noreply.github.com>
2021-04-22 08:36:44 +0600

    Add files via upload

---
+++ BasicC_Plus_Plus/importMath.cpp
@@ -0,0 +1,20 @@
+/*05
+Math funtions
+ */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout<<pow(2,5)<<endl;
+    cout<<sqrt(36)<<endl;
+    cout<<round(4.5)<<endl;
+    cout<<round(4.1)<<endl;
+    cout<<floor(3.66)<<endl;
+    cout<<ceil(3.66)<<endl;
+    cout<<fmax(10,20)endl;
+    cout<<fmin(10,20)endl;
+    return 0;
+}
+
