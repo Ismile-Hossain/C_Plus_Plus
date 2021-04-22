9f91d3c985288fe737773c5250376630957a4ce8
Md.Ismile Hossain <40804562+Ismile-Hossain@users.noreply.github.com>
2021-04-22 08:39:18 +0600

    Add files via upload

---
+++ BasicC_Plus_Plus/arithmaticOperator.cpp
@@ -0,0 +1,13 @@
+///06
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 6;
+  int y = 3;
+  cout << x + y<<endl;
+  cout << x - y<<endl;
+  cout << x * y<<endl;
+  cout << x/y<<endl;
+  return 0;
+}
